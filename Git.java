35b0eece2736fc1183453f39e03ee6f67f826306
Ian <ilieberman1@hwemail.com>
2024-09-19 21:29:52 -0700

    Added init method. Pretty sure it works but check with Theiss.

--- Git.java
+++ Git.java
@@ -1,5 +1,33 @@
+import java.io.*;
 public class Git {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        initRepo();
+    }
+    public static void initRepo() throws IOException
+    {
+        File file = new File("git");
+        File fileObj = new File("git/objects");
+        File fileText = new File ("git/objects/index");
+        if (file.exists() && fileObj.exists() && fileText.exists())
+        {
+            System.out.println("Git repository already exists");
+        }
+        else{
+        if (!file.exists())
+        {
+            file.mkdir();
+            
+        }
+        if(!fileObj.exists())
+            {
+                fileObj.mkdir();
+            }
+        if(!fileText.exists())
+            {
+                fileText.createNewFile();
+            }
+        }
+
 
     }
 }
