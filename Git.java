629ba8b14a3ed5821f6395f2e85a85c2b2fe671d
Ian <ilieberman1@hwemail.com>
2024-09-19 21:46:57 -0700

    Finished stretch goal 1

--- Git.java
+++ Git.java
@@ -1,7 +1,29 @@
 import java.io.*;
 public class Git {
     public static void main(String[] args) throws IOException {
+        
+        File file = new File("git");
+        File fileObj = new File("git/objects");
+        File fileText = new File ("git/objects/index");
+        if (fileText.exists()) //does kind of assume that fileText has to exist to delete anything, but I think it's fine because it's my tester, not a method. 
+        {
+            fileText.delete();
+            fileObj.delete();
+            file.delete();
+            if (!file.exists()&& !fileObj.exists() && !fileText.exists() )
+            {
+                System.out.println("deleted");
+            }
+            
+        }
         initRepo();
+        if (file.exists() && fileObj.exists() && fileText.exists())
+        {
+            System.out.println("Git repositories exist");
+        }
+        
+
+        //Need to finish stretch goal 1 here by checking for and deleting all the created directories and files
     }
     public static void initRepo() throws IOException
     {
@@ -30,4 +52,5 @@ public class Git {
 
 
     }
+
 }
