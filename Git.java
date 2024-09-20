f081edd455349e4708b0db0d1b848ca4e292a371
Ian <ilieberman1@hwemail.com>
2024-09-19 23:23:10 -0700

    Created findHash method, haven't checked for correctness yet.

--- Git.java
+++ Git.java
@@ -1,4 +1,8 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class Git {
     public static void main(String[] args) throws IOException {
         
@@ -51,6 +55,33 @@ public class Git {
         }
 
 
+    }
+    public static void createBlob(Path path) throws IOException, NoSuchAlgorithmException
+    {
+        String hash = findHash(path);
+
+        
+        
+        //read the file
+        //convert that to hash code
+        //put the file into the objects directory
+        //copy the text over
+    }
+
+    public static String findHash(Path path) throws IOException, NoSuchAlgorithmException
+    {
+        BufferedReader reader = Files.newBufferedReader(path);
+        String fileText = "";
+        while (reader.ready())
+        {
+            fileText += reader.readLine() + "\n"; // will this still work for the last line?
+        }
+        byte[] inputBytes = fileText.getBytes();
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        byte[] finalBytes = sha1.digest(inputBytes);
+        String finalHash = finalBytes.toString();
+        return finalHash;
+        //System.out.println(finalHash); //not going to be used laster on
     }
 
 }
