b9695abf7d9ef35514b4ac6019333c75f1283dc8
Christian <cstubbeman1@hwemail.com>
2024-10-02 19:04:51 -0700

    Started working on final tester with functionality connected

---
+++ FinalTester.java
@@ -0,0 +1,15 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+
+public class FinalTester {
+    public static void main(String[]args) throws NoSuchAlgorithmException, IOException
+    {
+        Git test = new Git();
+        test.initRepo();
+        
+    }
+}
